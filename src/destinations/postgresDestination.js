e53e8db22ee4fc79e8ccd991e7ff98b8fae2a65f
Charlie Benger-Stevenson <suityou01@yahoo.co.uk>
2024-08-14 15:49:01 +0100

    Added debug statements to postgresDestination

--- src/destinations/postgresDestination.js
+++ src/destinations/postgresDestination.js
@@ -1,6 +1,6 @@
-const { Transform } = require("node:stream")
+const { Transform } = require('node:stream')
 const { Sequelize } = require('sequelize')
-
+const debug = require('debug')('destination')
 /**
  * 
  * @param {Object} options 
@@ -22,14 +22,20 @@ function PostgresDestination(options){
     const host = options.host
     const mapping = options.mapping
     const includeErrors = options.includeErrors
+    let sequelize
+    try {
+        sequelize = new Sequelize(database, username, password, {
+            host: host,
+            dialect: 'postgres',
+            logging: false
+        })
 
-    const sequelize = new Sequelize(database, username, password, {
-        host: host,
-        dialect: 'postgres',
-        logging: false
-    })
-
-    sequelize.authenticate()
+        sequelize.authenticate()
+        debug('sequelize.authenticate succeeded')
+    } catch(e){
+        debug('sequelize.authenticate failed')
+        debug(e)
+    }
 
     function getMappingForColumn(column){
         const [map] = mapping.filter(m => m.column === column)
@@ -42,6 +48,7 @@ function PostgresDestination(options){
         })
         .join(",")}) VALUES (${chunk._columns.map((column,index) => {
             let mapping = getMappingForColumn(column)
+            if(!mapping) debug('Mapping not found for column %s', column)
             if(mapping.targetType === "varchar" || mapping.targetType === "char")
                 return `'${chunk[index]}'`
             return chunk[index]
@@ -55,11 +62,14 @@ function PostgresDestination(options){
             // @ts-ignore
             if(chunk.errors.length === 0 | options.includeErrors){
                 insertStatement = writeInsertStatement(chunk)
+                debug('Insert statement: [%s]', insertStatement)
                 sequelize.query(insertStatement)
                     .then(result => {
+                        debug('result %o', result)
                         chunk._result = result
                         callback(null, chunk)
                     }).catch(error => {
+                        debug('error %o', error)
                         chunk.errors.push(error)
                         callback(null, chunk)
                     })
--- test/destinations/postgresDestination.test.js
+++ test/destinations/postgresDestination.test.js
@@ -5,8 +5,8 @@ const { Sequelize } = require('sequelize')
 
 jest.mock("sequelize", () => ({
     Sequelize: jest.fn().mockReturnValue({
-        authenticate: jest.fn().mockReturnValue(true),
-        query: jest.fn().mockReturnValue(Promise.resolve([[],1]))
+        authenticate: jest.fn().mockResolvedValue(true),
+        query: jest.fn().mockResolvedValue([[],1])
     })
 }))
 
@@ -17,6 +17,10 @@ jest.mock('fs', () => ({
 ))
 
 describe('postgresDestination tests', () => {
+    beforeEach(()=> {
+        Sequelize.prototype.authenticate = jest.fn().mockResolvedValue(true)
+        Sequelize.prototype.query = jest.fn().mockResolvedValue([[],1])
+    })
     afterEach(() => {
         jest.resetAllMocks()
     })
@@ -54,8 +58,46 @@ describe('postgresDestination tests', () => {
         readable
             .pipe(uut)
             .on("data", (chunk) => {
-                console.log(chunk)
                 done()
             })
-    });
+    })
+    it('should produce debug output', (done) => {
+        const logSpy = jest.spyOn(process.stderr, 'write')
+        const uut = PostgresDestination({
+            username: "postgres",
+            password : "ppp",
+            database: "etl_db",
+            host: "postgres",
+            table: "target",
+            includeErrors: false,
+            mapping: [
+                {
+                    column: "column1",
+                    targetColumn: "target_column1",
+                    targetType: "varchar"
+                },
+                {
+                    column: "column2",
+                    targetColumn: "target_column2",
+                    targetType: "varchar"
+                },
+                {
+                    column: "column3",
+                    targetColumn: "target_column3",
+                    targetType: "varchar"
+                },
+            ]
+        })
+        const testData =["a", "b", "c"]
+        testData.errors = []
+        testData.rowId = 1
+        testData._columns = ["column1", "column2", "column3"]
+        const readable = Readable.from([testData])
+        readable
+            .pipe(uut)
+            .on("data", (chunk) => {
+                expect(logSpy).toBeCalledTimes(3)
+                done()
+            })
+    })
 });
