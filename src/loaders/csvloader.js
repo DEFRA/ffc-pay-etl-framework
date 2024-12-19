303e3ee42a281540826ead9cc2dec43048765929
Sam Plackett <samuel.plackett@eviden.com>
2024-12-19 12:28:31 +0000

    increase type safety

--- src/loaders/csvloader.js
+++ src/loaders/csvloader.js
@@ -3,12 +3,26 @@ const { Transform } = require("stream")
 const { parse } = require("csv-parse")
 
 /**
+ * @typedef {Object} CSVLoaderOptions
+ * @property {string} path The path to the CSV file
+ * @property {string[]} columns The columns in the CSV file
+ */
+
+/**
+ * @typedef {import('stream').Readable} Readable
+ */
+
+/**
+ * @typedef {Readable & {_columns?: string[], pump?: (csvLoader: Readable) => Readable}} CSVLoaderStream
+ */
+
+/**
  * 
- * @param {Object} options 
- * @param {String} options.path
- * @returns StreamReader
+ * @param {CSVLoaderOptions} options 
+ * @returns {CSVLoaderStream}
  */
 function CSVLoader(options){
+    /** @type {CSVLoaderStream} */
     let csvLoader = fs.createReadStream(options.path)
     let lineCount = 1
     csvLoader._columns = options.columns
@@ -28,14 +42,15 @@ function CSVLoader(options){
                     // remove non-printable characters
                     options.columns.forEach((_column, index) => {
                         if (chunk[index]) {
+                            console.log(`Before: ${chunk[index].split('').map(char => char.charCodeAt(0)).join(', ')}`)
                             chunk[index] = chunk[index].replace(/[\x00-\x1F\x7F-\x9F]/g, '')
+                            console.log(`After: ${chunk[index].split('').map(char => char.charCodeAt(0)).join(', ')}`)
                         }
                     })
 
                     callback(null, chunk)
                 }
-            })
-        )
+            }))
     }
     return csvLoader
 }
