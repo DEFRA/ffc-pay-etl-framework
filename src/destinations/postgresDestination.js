ec8290e7698fd6afd43e369d0e1a5bae265af839
Sam Plackett <samuel.plackett@eviden.com>
2025-04-08 15:35:42 +0100

    sorry

--- src/destinations/postgresDestination.js
+++ src/destinations/postgresDestination.js
@@ -24,7 +24,7 @@ function getReturningColumns(mapping){
 function writeInsertStatement(columnMapping, table, chunk, schema, ignoredColumns = []) {
     const filteredColumns = chunk._columns.filter(column => {
         const mapping = getMappingForColumn(columnMapping, column)
-        return !ignoredColumns.includes(mapping?.column)
+        return !ignoredColumns.includes(mapping?.targetColumn)
     })
     let statement = `INSERT INTO ${schema ?? 'public'}."${table}" (${filteredColumns.map(column => {
         const mapping = getMappingForColumn(columnMapping, column)
